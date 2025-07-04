0cdeeecfa345d7f6ef076db1bced715989fbcbdc
KTH <proxogus@naver.com>
2025-07-04 16:51:52 +0900

    로그인화면 디자인 및 header/footer 디자인 가져오는 부분 관련 기초셋팅진행.

---
+++ js/common.js
@@ -0,0 +1,50 @@
+let depthCount = 0;
+
+function loadHTML(selector, url) {
+    fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                return response.text();
+            } else {
+                return fetch('../' + url.replace('./', '')).then(
+                    (response2) => {
+                        if (response2.ok) {
+                            depthCount = 1;
+                            return response2.text();
+                        } else {
+                            throw new Error('Network response was not ok');
+                        }
+                    }
+                );
+            }
+        })
+        .then((data) => {
+            if (depthCount === 0) {
+                document.querySelector(selector).innerHTML = data;
+            } else {
+                const tempDiv = document.createElement('div');
+                tempDiv.innerHTML = data;
+                const imgs = tempDiv.querySelectorAll('img');
+                imgs.forEach((img) => {
+                    let src = img.getAttribute('src');
+                    if (src.startsWith('./')) {
+                        src = src.replace('./', '../'.repeat(depthCount));
+                    } else if (src.startsWith('/')) {
+                        src = src.replace('/', '../'.repeat(depthCount));
+                    } else if (src.startsWith('img')) {
+                        src = src.replace(
+                            'img',
+                            '../'.repeat(depthCount) + 'img'
+                        );
+                    }
+                    img.setAttribute('src', src);
+                });
+                document.querySelector(selector).innerHTML = tempDiv.innerHTML;
+            }
+        });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadHTML('#header', './includes/header.html');
+    loadHTML('#footer', './includes/footer.html');
+});
